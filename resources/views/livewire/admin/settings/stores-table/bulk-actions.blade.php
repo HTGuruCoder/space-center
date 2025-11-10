27abda562b4f18cb3b59a8d611ceb2beab912d57
HtGuruCoder <tchetcheherman@gmail.com>
2025-11-10 06:22:28 +0000

    u

--- resources/views/livewire/admin/settings/stores-table/bulk-actions.blade.php
+++ resources/views/livewire/admin/settings/stores-table/bulk-actions.blade.php
@@ -1,24 +1,15 @@
 @use(App\Enums\PermissionEnum)
 
-<div
-    x-data="{ count: 0 }"
-    x-init="
-        $watch('$wire.checkboxValues', value => {
-            count = value ? value.length : 0;
-        })
-    "
-    x-show="count > 0"
-    x-cloak
-    class="inline-flex items-center gap-2"
->
-    @can(PermissionEnum::DELETE_STORES->value)
+@can(PermissionEnum::DELETE_STORES->value)
+    <div class="inline-flex items-center gap-2">
         <button
             wire:click="$dispatch('bulkDeleteStores.{{ $tableName }}', [])"
             class="btn btn-error btn-sm"
+            :disabled="window.pgBulkActions.count('{{ $tableName }}') === 0"
         >
             <x-icon name="mdi.delete" class="w-4 h-4" />
             <span>{{ __('Delete') }}</span>
-            <span class="badge badge-sm" x-text="count"></span>
+            <span class="badge badge-sm" x-text="window.pgBulkActions.count('{{ $tableName }}')"></span>
         </button>
-    @endcan
-</div>
+    </div>
+@endcan
