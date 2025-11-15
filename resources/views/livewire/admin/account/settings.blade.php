faee4c64eb605e95f442059749c121426ec19dde
HtGuruCoder <tchetcheherman@gmail.com>
2025-11-15 19:54:26 +0000

    u

--- resources/views/livewire/admin/account/settings.blade.php
+++ resources/views/livewire/admin/account/settings.blade.php
@@ -79,6 +79,14 @@
                             required
                         />
 
+                        {{-- Country --}}
+                        <x-select
+                            label="{{ __('Country') }}"
+                            :options="CountryEnum::options()"
+                            wire:model="profileForm.country_code"
+                            placeholder="{{ __('Select a country') }}"
+                        />
+
                         {{-- Phone Number --}}
                         <x-input
                             label="{{ __('Phone Number') }}"
@@ -91,14 +99,6 @@
                             label="{{ __('Birth Date') }}"
                             type="date"
                             wire:model="profileForm.birth_date"
-                        />
-
-                        {{-- Country --}}
-                        <x-select
-                            label="{{ __('Country') }}"
-                            :options="CountryEnum::options()"
-                            wire:model="profileForm.country_code"
-                            placeholder="{{ __('Select a country') }}"
                         />
 
                         {{-- Timezone --}}
